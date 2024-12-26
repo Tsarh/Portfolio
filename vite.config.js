0cabed089da0101b724df24189f53ae5886a9ce6
Tsaroana <razjazz882@gmail.com>
2024-12-26 10:35:21 +0100

    modification on vite.config.js for the 'nom-de-depot'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/portfolio/', // Remplacez "nom-du-depot" par le nom de votre dépôt GitHub
+});
